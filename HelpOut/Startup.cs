e5e606da2ebd2c1b2c487b1ae29259886066b98c
Justin Zaner <justin.zaner@gmail.com>
2015-08-24 10:50:28 -0400

    Built Project
    Changed from WebApi to MVC

---
+++ HelpOut/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpOut.Startup))]
+namespace HelpOut
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
