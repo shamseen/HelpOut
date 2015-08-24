c3b66290adde4ec905e63f1d6c1dfde55186c1c5
shamseen <shamseen.c@gmail.com>
2015-08-24 16:07:46 -0400

    temp functionality for login

--- HelpOut/Controllers/HomeController.cs
+++ HelpOut/Controllers/HomeController.cs
@@ -20,6 +20,32 @@ namespace HelpOut.Controllers
 
         }
 
+        [HttpPost]
+        public ActionResult Index(LoginViewModel login)
+        {
+            ViewBag.Message = "This log in didn't break anything.";
+
+            ViewBag.isLoggedIn = false;
+
+            var q = from c in db.Users
+                    where login.Email == c.Email
+                    select c;
+            q = q.Where(u => u.Email.Equals(login.Email));
+
+            int count = q.Count(u => u.Email == u.Email);
+
+            if (count == 1 && q.First().Password == login.Password)
+            {
+                User user = (from c in q
+                            select c).First();
+
+                ViewBag.isLoggedIn = true;
+                ViewBag.Username = user.Email;
+                ViewBag.Name = user.FullName;
+            }
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
