895522fc84ad3f9d3921a763c208600403431c17
shamseen <shamseen.c@gmail.com>
2015-08-24 12:25:56 -0400

    Creating models and DAL

---
+++ HelpOut/DAL/HelpOutContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpOut.Models;
+using System.Data.Entity;
+
+
+namespace HelpOut.DAL
+{
+    public class HelpOutContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Event>()
+                .HasMany(c => c.Attendees).WithMany(i => i.EventsAttending)
+                .Map(t => t.MapLeftKey("EventID")
+                    .MapRightKey("UserID")
+                    .ToTable("Signups"));
+        }
+    }
+}
---
+++ HelpOut/Models/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpOut.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("User")] //will map to User's primary key (UserID)
+        public int OrganizationID { get; set; }
+
+
+        //navigation property
+        public User Organization { get; set; }
+        public virtual ICollection<User> Attendees { get; set; }
+
+        //[ForeignKey("UserID")]
+        //public User Organization { get; set; }
+    }
+}
