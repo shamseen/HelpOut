6984f936f91acdef9a709dfa7626f157c9eec1e8
Apatruno <Apatruno125@gmail.com>
2015-08-25 09:06:00 -0400

    Bootstrap file in mast
    Commit Bootstrap files

--- HelpOut/Migrations/Configuration.cs
+++ HelpOut/Migrations/Configuration.cs
@@ -27,22 +27,47 @@ namespace HelpOut.Migrations
                           Description = "I like long walks on the beach ",
                           Website = "",},
                 new User {Email = "Alonso@aol.com",
-                    Password = "Password",
-                    FullName = "Meredith Alsonso", 
-                    Location = "902 Water Front ct., Westland MI, 48186",
-                    PhoneNumber = "7347563304", 
-                    Description = "",
-                    Website = "",},
+                          Password = "Password",
+                          FullName = "Meredith Alsonso", 
+                          Location = "902 Water Front ct., Westland MI, 48186",
+                          PhoneNumber = "7347563304", 
+                          Description = "",
+                          Website = "",},
                  new User {Email = "Robertson@aol.com",
-                     Password = "Password",
-                     FullName = "Ashley Robinson",
-                     Location = "12 Chirrewa Lane, Detroit, MI, 48197",
-                     PhoneNumber = "8103390948", 
-                     Description = "",
-                     Website = "",},        
+                          Password = "Password",
+                          FullName = "Ashley Robinson",
+                          Location = "12 Chirrewa Lane, Detroit, MI, 48197",
+                          PhoneNumber = "8103390948", 
+                          Description = "",
+                          Website = "",},        
     };
             users.ForEach(s => context.Users.AddOrUpdate(p => p.FullName, s));
             context.SaveChanges();
+
+
+            var events = new List<Event>
+            {
+                new Event {EventID =1 ,
+                           Name ="Polar Plunge",
+                           DateTime = DateTime.Parse("02/23/26"),
+                           Location = "123 N. Normancy St.", 
+                           Description= " Join us on Feburary 23 2016 for the first Annual Polar Plunge! We will be looking for twelve Volunteers to hand out towels and give away hot chocolate." },
+                //new Event {Name ="Polar Plunge",
+                //    DateTime = DateTime.Parse("02/23/26"),
+                //    Location = "123 N. Normancy St.", 
+                //    Description= " Join us on Feburary 23 2016 for the first Annual Polar Plunge! We will be looking for twelve Volunteers to hand out towels and give away hot chocolate." },
+                // new Event {Email = "Robertson@aol.com",
+                //          Password = "Password",
+                //          FullName = "Ashley Robinson",
+                //          Location = "12 Chirrewa Lane, Detroit, MI, 48197",
+                //          PhoneNumber = "8103390948", 
+                //          Description = "",
+                //          Website = "",},        
+    };
+            events.ForEach(s => context.Events.AddOrUpdate(p => p.EventID, s));
+            context.SaveChanges();
+            //Create Events 
+
 
         }
     }
