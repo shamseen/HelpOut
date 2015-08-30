ddd824763630494a79c42343535d9c97e972cb42
shamseen <shamseen.c@gmail.com>
2015-08-30 12:21:00 -0400

    fixed create event (orgID now automatically bound)

--- HelpOut/Controllers/EventController.cs
+++ HelpOut/Controllers/EventController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using HelpOut.Models;
+using Microsoft.AspNet.Identity;
 
 namespace HelpOut.Controllers
 {
@@ -95,10 +96,12 @@ namespace HelpOut.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EventID,Name,DateTime,Location,Description,OrganizationID")] Event @event)
+        public ActionResult Create([Bind(Include = "EventID,Name,DateTime,Location,Description")] Event @event)
         {
+            
             if (ModelState.IsValid)
             {
+                @event.OrganizationID = User.Identity.GetUserId();
                 db2.Events.Add(@event);
                 db2.SaveChanges();
                 return RedirectToAction("Index");
