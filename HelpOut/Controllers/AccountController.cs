4a2df596f802a59ed86f8a0d251d408d02dba63b
shamseen <shamseen.c@gmail.com>
2015-08-30 15:01:14 -0400

    can register new account with roles BUT ..
    1. will get error if you try to make a new account with an existing
    email
    
    2. role selection isn't required..

--- HelpOut/Controllers/AccountController.cs
+++ HelpOut/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using HelpOut.Models;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace HelpOut.Controllers
 {
@@ -147,6 +149,9 @@ namespace HelpOut.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ApplicationDbContext context = new ApplicationDbContext();
+            ViewBag.Roles = new SelectList(context.Roles);
+            ViewBag.role = "";
             return View();
         }
 
@@ -155,7 +160,7 @@ namespace HelpOut.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(RegisterViewModel model, string selectedRole)
         {
             if (ModelState.IsValid)
             {
@@ -167,19 +172,35 @@ namespace HelpOut.Controllers
                     UserName = model.UserName, 
                     Email = model.Email,
                     PhoneNumber = model.PhoneNumber
-
                 };
+
+                switch (selectedRole)
+                {
+                    case "Organization": user.EventsCreated = new List<Event>(); break;
+                    default: user.EventsAttending = new List<Event>(); break;
+                }
+
                 var result = await UserManager.CreateAsync(user, model.Password);
+
+                //adding the user to a role here because it won't work in the if statement
+                var context = new ApplicationDbContext();
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                user.FullName = "itWorked";
+                userManager.AddToRole(user.Id, selectedRole);
+                //************************************************************************
+
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
+                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                    
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
