07f19e4f889cbc2250ab8894fd85b64331b411a3
Justin Zaner <justin.zaner@gmail.com>
2015-09-03 09:56:52 -0400

    Groundwork for PrintToPDF function
    Added nuget packages and AttendanceRoster class

---
+++ HelpOut/Models/AttendanceRoster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+using RazorPDF;
+
+namespace HelpOut.Models
+{
+    public class AttendanceRoster
+    {
+        public string Name { get; set; }
+        public virtual ICollection<ApplicationUser> Attendees { get; set; }
+        public bool? Present { get; set; }
+    }
+}
