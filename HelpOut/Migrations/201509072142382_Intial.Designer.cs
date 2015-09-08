37d4210b1baf8c4a2dd82c8436af8f1f8bd045e7
Apatruno <Apatruno125@gmail.com>
2015-09-07 21:36:39 -0400

    Allowing an External User to actually store a Role

--- HelpOut/Migrations/201509072142382_Intial.Designer.cs
+++ HelpOut/Migrations/201509072142382_Intial.Designer.cs
@@ -7,13 +7,13 @@ namespace HelpOut.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Intial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Intial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201509041756074_initial"; }
+            get { return "201509072142382_Intial"; }
         }
         
         string IMigrationMetadata.Source
